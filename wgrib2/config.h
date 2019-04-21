d69fcfba92de9a40a627ab2924258651e0de33b2
Martin Tang <neuks@qq.com>
2019-04-22 00:37:28 +0800

    initial upload
    Signed-off-by: Martin Tang <neuks@qq.com>

---
+++ wgrib2/config.h
@@ -0,0 +1,14 @@
+/* config.h */
+//#define DISABLE_ALARM
+//#define DISABLE_TIMEZONE
+//#define USE_REGEX
+//#define USE_PNG
+//#define USE_G2CLIB
+#define USE_TIGGE
+#define USE_OPENMP
+#define USE_IPOLATES 3
+#define CC "gcc version 7.3.0"
+#define GFORTRAN "GNU Fortran 7.3.0"
+#define IPOLATES_LIB "iplib.v3"
+#define BUILD_COMMENTS "MingW32 build"
+#define WMO_VALIDATION
